6b7a4f1d0cb1a93d534a455ff937cf20254ec825
Reshape <1742809983@qq.com>
2025-12-01 15:00:08 +0800

    first commit

---
+++ api/src/main/java/com/zql/Application.java
@@ -0,0 +1,14 @@
+package com.zql;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        SpringApplication.run(Application.class, args);
+    }
+}
