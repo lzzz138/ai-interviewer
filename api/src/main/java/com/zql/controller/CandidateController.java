77e5dec2738453a16c531349f50bc22f4b1d9fd1
Reshape <1742809983@qq.com>
2026-01-27 16:24:20 +0800

    还原CandidateController代码内容

--- api/src/main/java/com/zql/controller/CandidateController.java
+++ api/src/main/java/com/zql/controller/CandidateController.java
@@ -4,6 +4,7 @@ package com.zql.controller;
 import com.zql.bo.CandidateBo;
 import com.zql.bo.JobBo;
 import com.zql.grace.result.GraceJSONResult;
+import com.zql.pojo.Candidate;
 import com.zql.service.CandidateService;
 import com.zql.utils.PagedGridResult;
 import io.minio.messages.Grant;
@@ -29,6 +30,18 @@ public class CandidateController {
                                      @RequestParam(defaultValue = "10", name = "pageSize") Integer pageSize){
         PagedGridResult result = candidateService.queryList(realName, mobile, page, pageSize);
         return GraceJSONResult.ok(result);
+    }
+
+    @GetMapping("/detail")
+    public GraceJSONResult getDetail(String candidateId){
+        Candidate candidate = candidateService.getDetail(candidateId);
+        return GraceJSONResult.ok(candidate);
+    }
+
+    @PostMapping("/delete")
+    public GraceJSONResult delete(String candidateId){
+        candidateService.delete(candidateId);
+        return GraceJSONResult.ok();
     }
 
 }
